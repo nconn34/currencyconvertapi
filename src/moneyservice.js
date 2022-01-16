e3e78e0621e6004f7b34e75faec4c4774e29d514
Nathan Conn <nconn34@gmail.com>
2022-01-16 08:31:16 -0800

    Changed name of file to moneyservice.js

---
+++ src/moneyservice.js
@@ -0,0 +1,14 @@
+export default class MoneyService {  
+    static async getMoney() {
+      try {
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      } catch(error) {
+        return error.message;
+      }
+    }
+  }
+
