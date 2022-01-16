62cbe89b25ec2addfdc894ef203a17286034f302
Nathan Conn <nconn34@gmail.com>
2022-01-16 08:18:34 -0800

    Added final currencies

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { ExchangeProfile } from './currency.js';
-import MoneyService from './functions.js';
+import { ExchangeRates } from './currency.js';
+import MoneyService from './moneyservice.js';
 
 $(document).ready(function() {
 
-  $('#exchange_submit').click(function(event) {
+  $('#submit').click(function(event) {
       event.preventDefault();
-      const inputCurrency = $('#input_currency').val();
-      const inputAmount = $('#input_amount').val();
-      $('#exchange_results').html("Exchange Amount: ");
-      $('#input_amount').val("");
+      const currency = $('#currency').val();
+      const dollar = $('#dollar').val();
+      $('#exchange').html("Exchange Amount: ");
+      $('#dollar').val("");
       let promise = MoneyService.getMoney()
-      promise.then(function(rateResponse) {
-        let exchange1 = new ExchangeProfile(inputCurrency,inputAmount,rateResponse);
-        $('#exchange_results').append(`${exchange1.exchangeResults(inputCurrency,inputAmount)} ${inputCurrency}`);
+      promise.then(function(response) {
+        let exchange = new ExchangeRates(currency,dollar,response);
+        $('#exchange').append(`${exchange.exchangeRate(currency,dollar)} ${currency}`);
       });
     })
   });
