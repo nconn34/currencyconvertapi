b618f5f4253bf780a87af8acaf67af10462f37c4
Nathan Conn <nconn34@gmail.com>
2022-01-16 08:30:27 -0800

    Altered class to display different currencies

--- src/currency.js
+++ src/currency.js
@@ -1,19 +1,20 @@
-export class ExchangeProfile {
-    constructor(inputCurrency,inputAmount,exchangeRateObject) {
-      this.USD = exchangeRateObject.conversion_rates.USD,
-      this.EUR = exchangeRateObject.conversion_rates.EUR,
-      this.CHF = exchangeRateObject.conversion_rates.CHF,
-      this.JPY = exchangeRateObject.conversion_rates.JPY,
-      this.GBP = exchangeRateObject.conversion_rates.GBP,
-      this.AUD = exchangeRateObject.conversion_rates.AUD,
-      this.inputCurrency = inputCurrency,
-      this.inputAmount = inputAmount,
-      this.outputAmount = this.exchangeResults(this.inputCurrency,this.inputAmount)
+export class ExchangeRates {
+    constructor(currency,dollar,exchange) {
+      this.AUD = exchange.conversion_rates.AUD,
+      this.HKD = exchange.conversion_rates.HKD,
+      this.EUR = exchange.conversion_rates.EUR,
+      this.JPY = exchange.conversion_rates.JPY,
+      this.GBP = exchange.conversion_rates.GBP,
+      this.currency = currency,
+      this.dollar = dollar,
+      this.conversion = this.exchangeRate(this.currency,this.dollar)
     }
   
-    exchangeResults(inputCurrency,inputAmount) { 
-    let exchangeValue = this[inputCurrency];
-    let result = exchangeValue*inputAmount;
+    exchangeRate(currency,dollar) { 
+    let rate = this[currency];
+    let result = rate*dollar;
     return result;
     }
   }
+
+
